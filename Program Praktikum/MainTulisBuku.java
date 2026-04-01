16c7332818494ff702bb45853459fde73aea0030
Renth <135339655+Arch-Ren@users.noreply.github.com>
2026-04-01 17:33:49 +0700

    Delete Program Praktikum directory

--- Program Praktikum/MainTulisBuku.java
+++
@@ -1,6 +0,0 @@
-public class MainTulisBuku {
-    public static void main(String[] args) {
-        TulisBuku b1 = new TulisBuku();
-        System.out.println("Buku habis setelah " + b1.berapaLama() + " hari");
-    }
-}
--- Program Praktikum/TulisBuku.java
+++
@@ -1,8 +0,0 @@
-public class TulisBuku {
-    int lembar = 50;
-    int kata = 100;
-    
-    public int berapaLama() {
-        return 4*50;
-    }
-}
