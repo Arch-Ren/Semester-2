de2dbd1fcaa6aab8215d68ce1cb34a59d4a04e65
Renth <135339655+Arch-Ren@users.noreply.github.com>
2026-05-23 23:47:23 +0700

    Refactor

--- Modul11/DataTim.java
+++ Modul11/DataTim.java
@@ -3,6 +3,25 @@ package Modul11;
 import java.util.ArrayList;
 import java.util.List;
 
+class DataType {
+    String namaTim;
+    int noPemain;
+    int tinggiBadan; 
+    int beratBadan;
+
+    public DataType(String namaTim, int noPemain, int tinggiBadan, int beratBadan) {
+        this.namaTim = namaTim;
+        this.noPemain = noPemain;
+        this.tinggiBadan = tinggiBadan;
+        this.beratBadan = beratBadan;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %d -> TB: %d cm, BB: %d kg", namaTim, noPemain, tinggiBadan, beratBadan);
+    }
+}
+
 public class DataTim {
     public static List<DataType> ambilDataMentah() {
         List<DataType> daftarPemain = new ArrayList<>();
